7925655678385b38a30456f88b341c7275364b2d
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-08-09 16:40:07 +0530

    add bot message component

--- frontend/src/Camera.jsx
+++ frontend/src/Camera.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const CameraFeed = () => {
   return (
-    <div className="absolute top-30 left-[100px] right-30 w-[700px] h-100 bg-gray-900 rounded-lg overflow-hidden border-2 border-green-500">
+    <div className="absolute top-30 left-[100px] right-30 w-[700px] h-100 bg-black rounded-lg overflow-hidden border-2 border-green-500">
       {/* Camera feed content will go here */}
       <div className="w-full h-full flex items-center justify-center text-white">
         Camera Feed
